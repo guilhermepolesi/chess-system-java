0b80063c9d5b7f3e0247fa5fed53fc912e4ad9bc
guilhermepolesi <guilherme-polesi@hotmail.com>
2022-10-09 20:50:15 -0300

    Special move - Promotion

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -10,7 +10,7 @@ public class Pawn extends ChessPiece {
 	
 	private ChessMatch chessMatch;
 
-	public Pawn(Board board, Color color, ChessMatch chassMatch) {
+	public Pawn(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
 		this.chessMatch = chessMatch;
 	}
